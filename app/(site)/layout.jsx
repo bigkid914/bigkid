0d243b0cb42ad0cb83eafb6fb610b533b42f139c
simonandywilson <simon@soup.work>
2025-07-03 12:05:14 +0100

    Updated import paths for Header component to maintain consistency; formatted main element for improved readability.

--- app/(site)/layout.jsx
+++ app/(site)/layout.jsx
@@ -1,6 +1,6 @@
 import { draftMode } from "next/headers";
 import { Suspense } from "react";
-import { Header } from "@/components/header";
+import { Header } from "@/components/Header";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { VisualEditing } from "next-sanity";
 import { Toaster } from "sonner";
--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,7 +2,7 @@ import { Suspense } from "react";
 import { resolveOpenGraphImage } from "@/sanity/lib/utils";
 import { settingsQuery } from "@/sanity/lib/queries";
 import { sanityFetch } from "@/sanity/lib/live";
-import { Header } from "@/components/header";
+import { Header } from "@/components/Header";
 import { NotFound } from "@/components/NotFound";
 
 export async function generateMetadata() {
@@ -15,7 +15,8 @@ export async function generateMetadata() {
   return {
     title: {
       template: `%s — ${settings?.seo?.ogTitle}`,
-      default: settings?.seo?.ogTitle || process.env.NEXT_PUBLIC_PROJECT_NAME || "",
+      default:
+        settings?.seo?.ogTitle || process.env.NEXT_PUBLIC_PROJECT_NAME || "",
     },
     description: settings?.seo?.ogDescription,
     openGraph: {
@@ -30,8 +31,12 @@ export default async function NotFoundRoute() {
       <Suspense>
         <Header />
       </Suspense>
-      <main className={"min-h-[calc(100vh-170px)] w-screen overflow-x-hidden px-4 pb-4 font-serif font-sm bg-white"}>
-        <NotFound/>
+      <main
+        className={
+          "min-h-[calc(100vh-170px)] w-screen overflow-x-hidden px-4 pb-4 font-serif font-sm bg-white"
+        }
+      >
+        <NotFound />
       </main>
     </>
   );
